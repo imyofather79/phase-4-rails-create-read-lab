2f294b19bc0f3e2b2cc87cb0e3324a819a041a89
imyofather79 <chunsum708@gmail.com>
2021-12-05 23:12:07 -0700

    commit tree 109410638b34990ffc7755c1a332f5d341b73ae5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/plants', to: 'plants#index'
+  get '/plants/:id', to: 'plants#show'
+  post '/plants', to: 'plants#create'
+
+  # resources :plants, only: [:name, :image, :price]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
